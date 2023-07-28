1a955b5801da6f66c24a1fc196ddeee1db418682
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-07-27 23:13:36 -0300

    Criada a função exibirJogo(), ela exibe o o jogo com os espaços vazios.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem vindo ao Jogo da Velha");
+        System.out.println("Jogo da Velha!");
+        exibirJogo();
     }
+
+    private static final char campoLivre = ' ';
+
+    private static final char[][] jogo = {
+            { campoLivre, campoLivre, campoLivre },
+            { campoLivre, campoLivre, campoLivre },
+            { campoLivre, campoLivre, campoLivre }
+    };
+
+    public static void exibirJogo() {
+        int linha = 1;
+
+        System.out.println("    1   2   3");
+        for (int i = 0; i <= 2; i++) {
+            System.out.println("  -------------");
+            for (int j = 0; j <= 2; j++) {
+                if (j == 0) {
+                    System.out.print(linha);
+                    linha++;
+                }
+                System.out.print(" |" + (jogo[i][j]) + jogo[i][j]);
+            }
+            System.out.println(" |");
+        }
+        System.out.println("  -------------");
+    }
+
 }
